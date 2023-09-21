a1bccc5c9eccde6a82142d845b298d332f165106
JENkt4k <james.e.nelson@gmail.com>
2023-09-20 23:22:35 -0500

    update to metaball

--- gyroid.py
+++ gyroid.py
@@ -1,82 +1,94 @@
 import bpy
-import math
-import bmesh
+import numpy as np
 
-# Clear mesh objects in the scene
+# Clear mesh and metaball objects in the scene
 bpy.ops.object.select_all(action='DESELECT')
 bpy.ops.object.select_by_type(type='MESH')
+bpy.ops.object.select_by_type(type='META')
 bpy.ops.object.delete()
 
 # Initialize parameters
-L = 3.0  # size of the grid, in cm
-res = 20  # resolution
-thickness = 0.08  # thickness in cm (0.8 mm)
+#L = 3.0  # size of the grid, in cm
+#res = 10  # resolution
 
-# Create an empty mesh and link it to scene
-mesh = bpy.data.meshes.new(name="Gyroid")
-obj = bpy.data.objects.new("Gyroid", mesh)
+# Set the minimum dimension
+min_dim = 0.4  # For example, 0.1 cm or 1 mm
+thickness = min_dim  # thickness in cm (0.8 mm)
+
+# Derive other parameters
+L = 10 * min_dim  # Grid size
+res = 2* int(L / min_dim)  # Resolution
+#radius = res/L  # Metaball radius
+radius = 2*(L / (res))  # Metaball radius
+
+# Create a new metaball object
+mball = bpy.data.metaballs.new("GyroidMeta")
+obj = bpy.data.objects.new("GyroidMeta", mball)
 bpy.context.collection.objects.link(obj)
-bpy.context.view_layer.objects.active = obj
-obj.select_set(True)
 
-# Construct the mesh
-mesh = bpy.context.object.data
-bm = bmesh.new()
+# Create a grid using NumPy
+x, y, z = np.linspace(0, L, res), np.linspace(0, L, res), np.linspace(0, L, res)
+x, y, z = np.meshgrid(x, y, z)
 
+# Evaluate the gyroid equation using NumPy
+gyroid = np.sin(2 * np.pi * x / L) * np.cos(2 * np.pi * y / L) + \
+         np.sin(2 * np.pi * y / L) * np.cos(2 * np.pi * z / L) + \
+         np.sin(2 * np.pi * z / L) * np.cos(2 * np.pi * x / L)
+
+# Create a mask based on the thickness
+mask = np.abs(gyroid) < (thickness / 2)
+
+# Loop through the NumPy array to create metaballs
 for i in range(res):
     for j in range(res):
         for k in range(res):
-            x, y, z = i * (L / res), j * (L / res), k * (L / res)
-            
-            # Gyroid equation
-            gyroid_value = math.sin(2 * math.pi * x / L) * math.cos(2 * math.pi * y / L) + \
-                          math.sin(2 * math.pi * y / L) * math.cos(2 * math.pi * z / L) + \
-                          math.sin(2 * math.pi * z / L) * math.cos(2 * math.pi * x / L)
-            
-            # Create vertices where the gyroid equation is close to zero (within the thickness range)
-            if abs(gyroid_value) < (thickness / 2):
-                bmesh.ops.create_vert(bm, co=(x, y, z))
+            if mask[i, j, k]:
+                element = mball.elements.new(type='BALL')
+                element.co = (x[i, j, k], y[i, j, k], z[i, j, k])
+                element.radius = radius # 0.4  # You can adjust the radius
 
-# Update & Free BMesh
-bm.to_mesh(mesh)
-bm.free()
+# Update the scene
+bpy.context.view_layer.update()
 
 
 
 
-
-
-
+#import bpy
 #import numpy as np
-#import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
 
-## Parameters
+## Clear mesh and metaball objects in the scene
+#bpy.ops.object.select_all(action='DESELECT')
+#bpy.ops.object.select_by_type(type='MESH')
+#bpy.ops.object.select_by_type(type='META')
+#bpy.ops.object.delete()
+
+## Initialize parameters
 #L = 3.0  # size of the grid, in cm
-#res = 50  # resolution
+#res = 10  # resolution
 #thickness = 0.08  # thickness in cm (0.8 mm)
-#void_max = 3.0  # maximum void size in cm
 
-## Create a grid
+## Create a new metaball object
+#mball = bpy.data.metaballs.new("GyroidMeta")
+#obj = bpy.data.objects.new("GyroidMeta", mball)
+#bpy.context.collection.objects.link(obj)
+
+## Create a grid using NumPy
 #x, y, z = np.linspace(0, L, res), np.linspace(0, L, res), np.linspace(0, L, res)
 #x, y, z = np.meshgrid(x, y, z)
 
-## Gyroid equation
-#gyroid = np.sin(2*np.pi*x/L) * np.cos(2*np.pi*y/L) + \
-#         np.sin(2*np.pi*y/L) * np.cos(2*np.pi*z/L) + \
-#         np.sin(2*np.pi*z/L) * np.cos(2*np.pi*x/L)
+## Evaluate the gyroid equation using NumPy
+#gyroid = np.sin(2 * np.pi * x / L) * np.cos(2 * np.pi * y / L) + \
+#         np.sin(2 * np.pi * y / L) * np.cos(2 * np.pi * z / L) + \
+#         np.sin(2 * np.pi * z / L) * np.cos(2 * np.pi * x / L)
 
-## Create a mask for the gyroid surface based on the thickness
+## Create a mask based on the thickness
 #mask = np.abs(gyroid) < (thickness / 2)
 
-## Create a mask for the maximum void size
-#void_mask = gyroid > void_max
-
-## Combine the masks
-#final_mask = np.logical_and(mask, np.logical_not(void_mask))
-
-## Plotting (this is just for visualization)
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
-#ax.voxels(final_mask, edgecolor="k")
-#plt.show()
+## Loop through the NumPy array to create metaballs
+#for i in range(res):
+#    for j in range(res):
+#        for k in range(res):
+#            if mask[i, j, k]:
+#                element = mball.elements.new()
+#                element.co = (x[i, j, k], y[i, j, k], z[i, j, k])
+#                element.radius = 0.1  # You can adjust the radius
