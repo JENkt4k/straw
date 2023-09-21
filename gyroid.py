a2bccf3aa582e8deb68ba6a4f1983baba3b9d0d2
JENkt4k <james.e.nelson@gmail.com>
2023-09-20 23:24:11 -0500

    update to metaball working better

--- gyroid.py
+++ gyroid.py
@@ -1,94 +1,53 @@
+# working
+
 import bpy
 import numpy as np
 
-# Clear mesh and metaball objects in the scene
+# Clear existing mesh and metaball objects
 bpy.ops.object.select_all(action='DESELECT')
 bpy.ops.object.select_by_type(type='MESH')
 bpy.ops.object.select_by_type(type='META')
 bpy.ops.object.delete()
 
 # Initialize parameters
-#L = 3.0  # size of the grid, in cm
-#res = 10  # resolution
+L = 4.0  # size of the grid
+res = 40  # resolution
+thickness = 0.4  # thickness
 
-# Set the minimum dimension
-min_dim = 0.4  # For example, 0.1 cm or 1 mm
-thickness = min_dim  # thickness in cm (0.8 mm)
-
-# Derive other parameters
-L = 10 * min_dim  # Grid size
-res = 2* int(L / min_dim)  # Resolution
-#radius = res/L  # Metaball radius
-radius = 2*(L / (res))  # Metaball radius
-
-# Create a new metaball object
+# Create a new Metaball object
 mball = bpy.data.metaballs.new("GyroidMeta")
 obj = bpy.data.objects.new("GyroidMeta", mball)
 bpy.context.collection.objects.link(obj)
+bpy.context.view_layer.objects.active = obj
+obj.select_set(True)
 
-# Create a grid using NumPy
-x, y, z = np.linspace(0, L, res), np.linspace(0, L, res), np.linspace(0, L, res)
+# Generate grid using NumPy
+x, y, z = np.linspace(-L, L, res), np.linspace(-L, L, res), np.linspace(-L, L, res)
 x, y, z = np.meshgrid(x, y, z)
 
-# Evaluate the gyroid equation using NumPy
-gyroid = np.sin(2 * np.pi * x / L) * np.cos(2 * np.pi * y / L) + \
-         np.sin(2 * np.pi * y / L) * np.cos(2 * np.pi * z / L) + \
-         np.sin(2 * np.pi * z / L) * np.cos(2 * np.pi * x / L)
+# Evaluate gyroid equation
+gyroid = np.sin(x) * np.cos(y) + np.sin(y) * np.cos(z) + np.sin(z) * np.cos(x)
 
-# Create a mask based on the thickness
-mask = np.abs(gyroid) < (thickness / 2)
+# Create mask based on thickness
+mask = np.abs(gyroid) < thickness
 
-# Loop through the NumPy array to create metaballs
+# Create metaballs based on mask
 for i in range(res):
     for j in range(res):
         for k in range(res):
             if mask[i, j, k]:
                 element = mball.elements.new(type='BALL')
                 element.co = (x[i, j, k], y[i, j, k], z[i, j, k])
-                element.radius = radius # 0.4  # You can adjust the radius
+                element.radius = 0.4  # Adjust the radius as needed
 
-# Update the scene
+# Update scene
 bpy.context.view_layer.update()
 
+# end working
 
+## Convert to mesh
+#try:
+#    bpy.ops.object.convert(target='MESH')
+#except Exception as e:
+#    print(f"Failed to convert metaball to mesh: {e}")
 
-
-#import bpy
-#import numpy as np
-
-## Clear mesh and metaball objects in the scene
-#bpy.ops.object.select_all(action='DESELECT')
-#bpy.ops.object.select_by_type(type='MESH')
-#bpy.ops.object.select_by_type(type='META')
-#bpy.ops.object.delete()
-
-## Initialize parameters
-#L = 3.0  # size of the grid, in cm
-#res = 10  # resolution
-#thickness = 0.08  # thickness in cm (0.8 mm)
-
-## Create a new metaball object
-#mball = bpy.data.metaballs.new("GyroidMeta")
-#obj = bpy.data.objects.new("GyroidMeta", mball)
-#bpy.context.collection.objects.link(obj)
-
-## Create a grid using NumPy
-#x, y, z = np.linspace(0, L, res), np.linspace(0, L, res), np.linspace(0, L, res)
-#x, y, z = np.meshgrid(x, y, z)
-
-## Evaluate the gyroid equation using NumPy
-#gyroid = np.sin(2 * np.pi * x / L) * np.cos(2 * np.pi * y / L) + \
-#         np.sin(2 * np.pi * y / L) * np.cos(2 * np.pi * z / L) + \
-#         np.sin(2 * np.pi * z / L) * np.cos(2 * np.pi * x / L)
-
-## Create a mask based on the thickness
-#mask = np.abs(gyroid) < (thickness / 2)
-
-## Loop through the NumPy array to create metaballs
-#for i in range(res):
-#    for j in range(res):
-#        for k in range(res):
-#            if mask[i, j, k]:
-#                element = mball.elements.new()
-#                element.co = (x[i, j, k], y[i, j, k], z[i, j, k])
-#                element.radius = 0.1  # You can adjust the radius
